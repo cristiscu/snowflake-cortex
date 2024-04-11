93c5d1f775486fce532c1684ee280d4f38add74e
cristiscu <cristiscu@gmail.com>
2024-04-11 11:46:45 -0700

    data

---
+++ 2-ml-pipelines-on-datasets/1-data-collection/2-data-access/1-pandas-decompress.py
@@ -0,0 +1,16 @@
+# see https://github.com/Snowflake-Labs/sfguide-snowpark-scikit-learn/blob/main/1_snowpark_housing_data_ingest.ipynb
+
+import os, tarfile, urllib.request
+
+REMOTE = "https://raw.githubusercontent.com/ageron/handson-ml2/master/datasets/housing"
+LOCAL = "../../.spool/datasets"
+
+# download remote TAR file
+urllib.request.urlretrieve(f"{REMOTE}/housing.tgz", f"{LOCAL}/housing.tgz")
+
+# decompress into local CSV
+with tarfile.open(f"{LOCAL}/housing.tgz") as file:
+    file.extractall(path=LOCAL, filter="fully_trusted")
+
+# delete local archive
+os.remove(f"{LOCAL}/housing.tgz")
--- 2-ml-pipelines-on-datasets/1-data-collection/2-pandas-read-csv.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd 
-
-df  = pd.read_csv("../../.spool/datasets/housing.csv") 
-print(df.head())
